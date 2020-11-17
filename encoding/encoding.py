c57894a0d1117d03385ca913e9112aacc9ed19dc
augub <augustijndeboer@gmail.com>
2020-11-17 17:16:08 +0100

    Working on converting the spectrogram to a spike pattern

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -6,14 +6,17 @@ from python_speech_features import logfbank
 import matplotlib.pyplot as plt
 
 data = loadmat("data/TIDIGIT_test.mat")
-sample = data['test_samples'][1][0]
+sample = data['test_samples'][0][0]
 
 samplerate = 16000
 duration = len(sample)/samplerate
-number_of_rows = 100
+number_of_rows = 41
+n_frequency_bands = 40
 winstep = duration/number_of_rows
-fbank = logfbank(sample,samplerate=samplerate, winlen = 2*winstep, winstep = winstep)
+fbank = logfbank(sample , samplerate = samplerate , winlen = 2 * winstep, winstep = winstep, nfilt = n_frequency_bands)
 
 
+number_of_timesteps = 30
+print(fbank)
 # plt.imshow(fbank)
 # plt.show()
