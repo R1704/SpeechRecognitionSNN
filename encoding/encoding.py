41c17329016462362db59b4d970dfdff84dc5b7f
augub <augustijndeboer@gmail.com>
2020-11-17 16:49:04 +0100

    Made the mfsc output fixed size spectrograms

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import scipy as sp
 from scipy.io import loadmat
-import matplotlib.pyplot as plt
-data = loadmat("data/TIDIGIT_test.mat")
+from python_speech_features import logfbank
 
-plt.plot(data['test_samples'][2][0])
-plt.show()
+import matplotlib.pyplot as plt
+
+data = loadmat("data/TIDIGIT_test.mat")
+sample = data['test_samples'][1][0]
+
+samplerate = 16000
+duration = len(sample)/samplerate
+number_of_rows = 100
+winstep = duration/number_of_rows
+fbank = logfbank(sample,samplerate=samplerate, winlen = 2*winstep, winstep = winstep)
+
+
+# plt.imshow(fbank)
+# plt.show()
