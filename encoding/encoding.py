f9aca121602a5e2a2a31f2bb3c1f95d83713442e
augub <augustijndeboer@gmail.com>
2020-11-18 11:01:04 +0100

    Encoded the data, read the ttfs_spikes_README file

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -3,60 +3,62 @@ import numpy as np
 from scipy.io import loadmat
 from python_speech_features import logfbank
 import matplotlib.pyplot as plt
+import pickle
 
-data = loadmat("data/TIDIGIT_test.mat")
-samples = data['test_samples']
+data_paths = ["test","train"]
+for path in data_paths:
 
-n_samples = len(samples)
-samplerate = 20000
-n_rows = 40
-n_frequency_bands = 40
-n_spiketime_bins = 30
-nfft = 1024
+    data = loadmat(f"data/TIDIGIT_{path}.mat")
+    samples = data[f'{path}_samples']
 
-def spectrogram(sample):
+    n_samples = len(samples)
+    samplerate = 20000
+    n_rows = 41
+    n_frequency_bands = 40
+    n_spiketime_bins = 30
+    nfft = 1024
+
+    def spectrogram(sample):
+        """
+        function to convert a signal to a spectrogram
+        """
+        duration = len(sample[0])/samplerate
+        winstep = duration/n_rows
+        fbank = logfbank(sample[0], samplerate=samplerate, winlen=1.1*
+                        winstep, winstep=winstep, nfilt=n_frequency_bands, nfft=nfft)
+        return fbank
+
+    def spikepattern(spectrogram, bins):
+        """
+        function to convert spectrogram to (categorical) spikes, given the bins    
+        """
+        return np.digitize(spectrogram, bins)
+
+    # Create spectrograms for each signal
+    spectrograms = np.zeros((n_samples, n_rows, n_frequency_bands))
+    for si, sample in enumerate(samples):
+        if si%10 == 0:
+            print(f"{si}th out of {n_samples} {path} samples \t spectrogram created\r", end = "")
+        spect = spectrogram(sample)
+        spectrograms[si] = spect
+
     """
-    function to convert a signal to a spectrogram
+    Construct the bins using global statistics. This is not made explicit in the paper,
+    and it is easily adjusted to only consider local statistics.
     """
-    duration = len(sample[0])/samplerate
-    winstep = duration/n_rows
-    fbank = logfbank(sample[0], samplerate=samplerate, winlen=1.1*
-                     winstep, winstep=winstep, nfilt=n_frequency_bands, nfft=nfft)
-    return fbank
+    max_amplitude = np.max(spectrograms) # global max
+    min_amplitude = np.min(spectrograms) # global min
+    bins = np.linspace(min_amplitude, max_amplitude, n_spiketime_bins+1) # bins are evenly spaced
 
-def spikepattern(spectrogram, bins):
-    """
-    function to convert spectrogram to spikes, given the bins    
-    """
-    sample_spikes = np.zeros((n_rows, n_frequency_bands, n_spiketime_bins))
-    for ri, row in enumerate(spectrogram):
-        row_spikes = pd.get_dummies(pd.cut(row, bins=bins))
-        sample_spikes[ri] = row_spikes.to_numpy()
-    return sample_spikes
+    # Create spike pattern for each spectrogram
+    spike_patterns = np.zeros((n_samples, n_rows, n_frequency_bands))
+    for si, spectrogram in enumerate(spectrograms):
 
-# Create spectrograms for each signal
-spectrograms = np.zeros((n_samples, n_rows, n_frequency_bands))
-for si, sample in enumerate(samples):
-    if si%10 == 0:
-        print(f"{si}th out of {n_samples} \t spectrogram created\r", end = "")
-    spect = spectrogram(sample)
-    spectrograms[si] = spect
+        spike_patterns[si] = spikepattern(spectrogram, bins)
+        if si%10 == 0:
+            print(f"{si}th out of {n_samples} {path} samples \t spike pattern created\r", end = "")
 
-"""
-Construct the bins using global statistics. This is not made explicit in the paper,
-and it is easily adjusted to only consider local statistics.
-"""
-max_amplitude = np.max(spectrograms) # global max
-min_amplitude = np.min(spectrograms) # global min
-bins = np.linspace(min_amplitude, max_amplitude, n_spiketime_bins+1) # bins are evenly spaced
-
-# Create spike pattern for each spectrogram
-spike_patterns = np.zeros((n_samples, n_rows, n_frequency_bands, n_spiketime_bins))
-for si, spectrogram in enumerate(spectrograms):
-    if si%10 == 0:
-        print(f"{si}th out of {n_samples} \t spike pattern created\r", end = "")
-    spike_patterns[si] = spikepattern(spectrogram, bins)
-
+    pickle.dump(spike_patterns, open(f"data/ttfs_spikes_{path}.p", "wb"))
 
 # sample = samples[0, 0]
 # spectrogram = spectrograms[0]
