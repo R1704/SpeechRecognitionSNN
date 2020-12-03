72fd4516f64813c188419da3475d9407b47f0192
augub <augustijndeboer@gmail.com>
2020-12-03 09:57:36 +0100

    Working on the training loop

--- playground_folder/convnet.py
+++ playground_folder/convnet.py
@@ -6,33 +6,44 @@ import torch.nn
 class Convnet(nn.Module):
     def __init__(self, input_size, n_feature_maps, kernel_size, n_sections,section_length_postsyn,n_channels):
         super(Convnet, self).__init__()
-        self.n_timesteps, self.frequency_bands = input_size
-        self.kernel_timesize,self.kernel_frequencysize = kernel_size
-        self.n_feature_maps = n_feature_maps
-        self.kernel_size = kernel_size
-        self.n_sections = n_sections
-        self.section_length_postsyn = section_length_postsyn
-        self.section_length_presyn = self.kernel_timesize+section_length_postsyn-1
-        self.n_channels = n_channels
-        self.overlap = ((self.section_length_presyn * self.n_sections) - self.n_timesteps)/(n_sections-1)
-        self.section_distance = self.section_length_presyn - self.overlap
-        self.convlayers = torch.nn.ModuleList([Convolution(1,self.n_channels,(self.kernel_timesize,self.kernel_frequencysize)) for c in range(n_sections)])
-        self.stdps = torch.nn.ModuleList([STDP(i, (0.004, -0.003)) for i in self.convlayers])
-        self.poollayers = torch.nn.ModuleList([Pooling((self.section_length, self.kernel_frequencysize)) for c in range(n_sections)])
+        self.n_timesteps, self.frequency_bands          = input_size
+        self.kernel_timesize,self.kernel_frequencysize  = kernel_size
+        self.n_feature_maps                             = n_feature_maps
+        self.kernel_size                                = kernel_size
+        self.n_sections                                 = n_sections
+        self.section_length_postsyn                     = section_length_postsyn
+        self.section_length_presyn                      = self.kernel_timesize+section_length_postsyn-1
+        self.n_channels                                 = n_channels
+        self.overlap                                    = ((self.section_length_presyn * self.n_sections) - self.n_timesteps)/(n_sections-1)
+        self.section_distance                           = self.section_length_presyn - self.overlap
 
+        self.convlayers = torch.nn.ModuleList([Convolution(1,self.n_channels,(self.kernel_timesize,self.kernel_frequencysize)) for c in range(n_sections)])
+
+        self.stdps = torch.nn.ModuleList([STDP(i, (0.004, -0.003)) for i in self.convlayers])
+
+        self.poollayers = torch.nn.ModuleList([Pooling((self.section_length, self.kernel_frequencysize)) for c in range(n_sections)])
 
     def forward(self,X):
         if not self.training:
-            X_sections = [X[(i*self.section_distance):(i*self.section_distance)+self.section_length_presyn] for i in range(self.n_sections)]
-            section_pots = [self.convlayers[i](X_sections[i]) for i in range(self.n_sections)]
-            spikes = [sf.fire(section_pots[i], 15) for i in range(self.n_sections)]
-            pools = [self.poollayers[i](spikes[i]) for i in range(self.n_sections)]
-            pool = torch.stack(pools,dim=0)
-            winners = sf.get_k_winners(pool)
-            output = -1
+            X_sections      = [X[(i*self.section_distance):(i*self.section_distance)+self.section_length_presyn] for i in range(self.n_sections)]
+            section_pots    = [self.convlayers[i](X_sections[i]) for i in range(self.n_sections)]
+            spikes          = [sf.fire(section_pots[i], 15) for i in range(self.n_sections)]
+            pools           = [self.poollayers[i](spikes[i]) for i in range(self.n_sections)]
+            pool            = torch.stack(pools,dim=0)
+            winners         = sf.get_k_winners(pool)
+            output          = -1
             if len(winners)!=0:
                 output = winners[0][0]
-
+            return output, pool
+        elif self.training:
+            X_sections      = [X[(i*self.section_distance):(i*self.section_distance)+self.section_length_presyn] for i in range(self.n_sections)]
+            section_pots    = [self.convlayers[i](X_sections[i]) for i in range(self.n_sections)]
+            spikes_pots     = [sf.fire(section_pots[i], 15) for i in range(self.n_sections)]
+            winners         = [sf.get_k_winners(sp[1]) for s in spikes_pots ]
+            self.save_data(X_sections, [sp[0] for sp in spikes_pots], [sp[1] for sp in spikes_pots], winners)
+            output          = -1
+            if len(winners)!=0:
+                output = winners[0][0]
             return output
 
 
@@ -42,4 +53,13 @@ class Convnet(nn.Module):
         self.ctx["potentials"] = pot
         self.ctx["output_spikes"] = spk
         self.ctx["winners"] = winners
+
+    def stdp(self):
+        input_spks= self.ctx["input_spikes"]
+        pots = self.ctx["potentials"]
+        spks= self.ctx["output_spikes"]
+        winnerss = self.ctx["winners"]
+
+
+        for i in len(input_spikes):
 
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import bindsnet as bn
 from playground_folder.convnet import Convnet
 
-input_size, n_feature_maps, kernel_size, stride, n_sections, section_length = (41,40), 50,(6,40),1,9,4
-net = Convnet(input_size, n_feature_maps, kernel_size, stride, n_sections, section_length)
+input_size, n_feature_maps, kernel_size,  n_sections, section_length_postsyn = (41, 40), 50, (6, 40),  9, 4
+net = Convnet(input_size, n_feature_maps, kernel_size,  n_sections, section_length_postsyn)
+
+net.train()
+
+for i in range(len(data)):
+    data_in = data[i]
+    net(data_in)
+    net.stdp()
