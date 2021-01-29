c445ec6e49037f51e069bf02aa3bd6cefe9ce00c
R1704 <32569659+R1704@users.noreply.github.com>
2021-01-29 22:04:51 +0100

    Add files via upload

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -1,77 +1,194 @@
 import pandas as pd
-import numpy as np
 from scipy.io import loadmat
+import os
 from python_speech_features import logfbank
 import matplotlib.pyplot as plt
+import numpy as np
+import librosa
+import librosa.display
 import pickle
-import os
+import math
+import glob
+import regex as rgx
+from pathlib import Path
 
-data_paths = ["test","train"]
-for path in data_paths:
+
+
+n_time_frames = 41
+n_frequency_bands = 40
+n_mels = 40
+n_spiketime_bins = 31
+hop_length = 512
+threshold = 0.001  # to trim the signal
+a_little_bit = 0.00000001
+
+
+def spikepattern_local(spectrogram):
+    max_amplitude = np.max(spectrogram)
+    min_amplitude = np.min(spectrogram)
+    bins = np.linspace(min_amplitude, max_amplitude, n_spiketime_bins + 1)  # bins are evenly spaced
+    return np.digitize(spectrogram, bins)
+
+# def spectrogram(sample, sample_rate, n_fft):
+#
+#     # Calculate hop length
+#     duration = sample.shape[0]
+#     hop_length = duration / n_time_frames
+#     hop_length = math.ceil(hop_length) if hop_length % 1 != 0 else math.ceil(hop_length + a_little_bit)
+#
+#     # Create Spectrogram from Mel bands
+#     mel = librosa.feature.melspectrogram(sample, sr=sample_rate, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+#     spect = librosa.power_to_db(mel, ref=np.max)
+#     return spect.T
+
+
+samplerate = 20000
+n_rows = 41
+n_frequency_bands = 40
+n_spiketime_bins = 30
+nfft = 2048
+
+def spectrogram(sample):
+    """
+    function to convert a signal to a spectrogram
+    """
+    duration = len(sample)/samplerate
+    winstep = duration/n_rows
+    fbank = logfbank(sample, samplerate=samplerate, winlen=1.1*
+                    winstep, winstep=winstep, nfilt=n_frequency_bands, nfft=nfft)
+    return fbank
+
+
+def get_TIMIT(path, save=False):
+    """
+    This function goes through all directories of the TIMIT data set and creates a pickle which contains a dictionary
+    of two lists. One list contains all labels and the other all spike patterns. Each sentence of the original
+    data-set is chopped up into single words.
+    Args:
+        path: test or train
+        save: makes a pickle if True
+
+    Returns:
+        the data
+    """
+    sample_rate = 16000
+    n_fft = 512
+    data = {f'{path}_samples': [], f'{path}_labels': []}
+    words = ['that', 'she', 'all', 'your', 'me', 'had', 'like', "don't", 'year', 'water', 'dark', 'rag', 'oily', 'wash', 'ask', 'carry', 'suit']
+
+    # Search through directories recursively
+    for filename in glob.iglob(f'data/TIMIT/{path}' + '/**/*.WRD', recursive=True):
+        wav_path = filename[:-3] + 'WAV'
+
+        # Get the sentence and the waveform
+        with open(filename, 'r') as f:
+            sentence = f.read().split('\n')[:-1]
+            audio, _ = librosa.load(wav_path, sr=sample_rate)
+
+            # For each word in the sentence make a spike pattern and label
+            for start_end_word in sentence:
+                start, end, word = tuple(start_end_word.split(' '))
+
+                if word in words:
+
+                    if int(start) - int(end) != 0:
+
+                        # Chop up sentence to single words
+                        chopped = audio[int(start):int(end)]
+
+                        # Show original vs chopped
+                        fig, ax = plt.subplots(2, 1)
+                        ax[0].plot(audio)
+                        ax[1].plot(chopped)
+                        plt.show()
+                        print(start, end, word)
+
+                        # Make the spectogram and spike pattern
+                        spect = spectrogram(chopped, sample_rate, n_fft)
+                        spike_pattern = spikepattern_local(spect)
+                        if spect.shape == (41, 40):
+
+                            # Show the spectogram
+                            plt.imshow(spect)
+                            plt.show()
+
+                            # Save label
+                            data[f'{path}_labels'].append(word)
+
+                            # Save spike pattern
+                            data[f'{path}_samples'].append(spike_pattern)
+    if save:
+        print(f'Saving TIMIT_mel_{path}')
+        pickle.dump(data, open(f"ttfs_spikes_data/TIMIT_mel_v2_{path}.p", "wb"))
+
+    return data
+
+
+def get_TIDIGITS(path, save=False):
+    sample_rate = 20000
+    n_fft = 2048
 
     data = loadmat(f"{os.getcwd()}/data/TIDIGIT_{path}.mat")
     samples = data[f'{path}_samples']
-
     n_samples = len(samples)
-    samplerate = 20000
-    n_rows = 41
-    n_frequency_bands = 40
-    n_spiketime_bins = 30
-    nfft = 1024
+    sample_avg_len = 0
 
-    def spectrogram(sample):
-        """
-        function to convert a signal to a spectrogram
-        """
-        duration = len(sample[0])/samplerate
-        winstep = duration/n_rows
-        fbank = logfbank(sample[0], samplerate=samplerate, winlen=1.1*
-                        winstep, winstep=winstep, nfilt=n_frequency_bands, nfft=nfft)
-        return fbank
+    spectrograms = np.zeros((n_samples, n_time_frames, n_frequency_bands))
+    for i, sample in enumerate(samples):
+        if i % 10 == 0:
+            print(f"{i}th out of {n_samples} {path} samples \t spectrogram created")
 
-    def spikepattern(spectrogram, bins):
-        """
-        function to convert spectrogram to (categorical) spikes, given the bins    
-        """
-        return np.digitize(spectrogram, bins)
+        sample = sample[0].flatten()
 
-    def spikepattern_local(spectrogram):
-        max_amplitude = np.max(spectrogram)  # global max
-        min_amplitude = np.min(spectrogram)  # global min
-        bins = np.linspace(min_amplitude, max_amplitude, n_spiketime_bins + 1)  # bins are evenly spaced
-        return np.digitize(spectrogram, bins)
+        # Trim and plot
+        indx = np.where(abs(sample) > threshold)
+        fig, ax = plt.subplots(2, 1)
+        ax[0].plot(sample)
+        sample = sample[indx]
+        ax[1].plot(sample)
+        plt.show()
 
-    # Create spectrograms for each signal
-    spectrograms = np.zeros((n_samples, n_rows, n_frequency_bands))
-    for si, sample in enumerate(samples):
-        if si%10 == 0:
-            print(f"{si}th out of {n_samples} {path} samples \t spectrogram created")
+        sample_avg_len += len(sample)
+
+        # spect = spectrogram(sample, sample_rate, n_fft)
         spect = spectrogram(sample)
 
-        spectrograms[si] = spect
+        if spect.shape == (41, 40):
+            # Save spectrogram
+            spectrograms[i] = spect
 
-    # """
-    # Construct the bins using global statistics. This is not made explicit in the paper,
-    # and it is easily adjusted to only consider local statistics.
-    # """
-    # max_amplitude = np.max(spectrograms) # global max
-    # min_amplitude = np.min(spectrograms) # global min
-    # bins = np.linspace(min_amplitude, max_amplitude, n_spiketime_bins+1) # bins are evenly spaced
+        # Plot spectrogram
+        plt.imshow(spect)
+        plt.show()
 
-    # Create spike pattern for each spectrogram
-    spike_patterns = np.zeros((n_samples, n_rows, n_frequency_bands))
-    for si, spectrogram in enumerate(spectrograms):
-        spike_patterns[si] = spikepattern_local(spectrogram)
-        if si%10 == 0:
-            print(f"{si}th out of {n_samples} {path} samples \t spike pattern created\r", end = "")
-    pickle.dump(spike_patterns, open(f"data/ttfs_spikes_v2_{path}.p", "wb"))
+    spike_patterns = np.zeros((n_samples, n_time_frames, n_frequency_bands))
+    for i, spect in enumerate(spectrograms):
+        spike_patterns[i] = spikepattern_local(spect)
+        if i % 10 == 0:
+            print(f"{i}th out of {n_samples} {path} samples \t spike pattern created\r", end="")
+    if save:
+        pickle.dump(spike_patterns, open(f"ttfs_spikes_data/TIGIDITS_spikes_{path}.p", "wb"))
 
-# sample = samples[0, 0]
-# spectrogram = spectrograms[0]
-# spikepattern = spike_patterns[0][20]
+    print(sample_avg_len, n_samples, sample_avg_len/ n_samples)
 
-# fig, ax = plt.subplots(1, 3)
-# ax[0].plot(sample)
-# ax[1].imshow(spectrogram.T)
-# ax[2].imshow(spikepattern, cmap="Greys")
-# plt.show()
+    return spike_patterns
+
+
+def run():
+    paths = ['train', 'test']
+    results = []
+    for path in paths:
+        print(f'starting with {path} data')
+        result = get_TIDIGITS(path, save=False)
+        results.append(result)
+
+    return results
+# run()
+
+
+
+
+
+
+
+
